a395a573fa6b4df08ca7384d2bc5eb1a442eb6d7
wyzhang <wyzhang@sepc622.se.cuhk.edu.hk>
2023-10-09 12:07:54 +0800

    delete useless classes

--- trainer.py
+++ trainer.py
@@ -1,95 +1,13 @@
 import tensorflow as tf
-from solvers import BaseBSDESolver, BermudanSolver, EuropeanSolver, FixIncomeEuropeanSolver
-from data_generators import DiffusionModelGenerator
-from longstaff_solver import LongStaffSolver
+from solvers import EuropeanSolver, FixIncomeEuropeanSolver
 from function_space import DeepONet, DeepKernelONetwithPI, DeepKernelONetwithoutPI
 from options import BaseOption
-from typing import List, Tuple
 from sde import ItoProcessDriver, HullWhiteModel
 class LossHistory(tf.keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.losses = []
     def on_batch_end(self, batch, logs={}):
         self.losses.append(logs.get('loss'))
-
-class BSDETrainer:
-    def __init__(self, solver: BaseBSDESolver):
-        self.solver = solver
-        self.eqn_config = self.solver.eqn_config
-        self.net_config = self.solver.net_config
-        self.option = self.solver.option
-        self.sde = self.solver.sde
-
-    def pre_setting(self):
-        learning_rate = self.net_config.lr
-        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate=learning_rate,
-            decay_steps=200,
-            decay_rate=0.9
-        )
-        Optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule, epsilon=1e-6)
-        self.data_generator = DiffusionModelGenerator(self.sde, self.eqn_config, self.option, 100)
-        # self.val_generator = DiffusionModelGenerator(self.sde, self.config, self.option, 20)
-        self.solver.compile(optimizer=Optimizer)
-
-    # def train(self, nr_epochs: int, checkpoint_path: str):
-    #     self.pre_setting()
-    #     # Create a callback that saves the model's batch loss
-    #     class LossHistory(tf.keras.callbacks.Callback):
-    #         def on_train_begin(self, logs={}):
-    #             self.losses = []
-    #         def on_batch_end(self, batch, logs={}):
-    #             self.losses.append(logs.get('loss'))
-    #     history = LossHistory()
-    #     self.solver.fit(x=self.data_generator, epochs=nr_epochs, callbacks=[history])
-    #     self.solver.no_net.save_weights(checkpoint_path)
-    #     # return history
-    #     return history
-    
-    def train(self, nr_epochs: int, dataset_path: str, checkpoint_path: str):
-        self.pre_setting()
-        # Create a callback that saves the model's batch loss
-        
-        history = LossHistory()
-        # load dataset
-        samples = self.eqn_config.sample_size
-        time_steps = self.eqn_config.time_steps
-        dims = self.eqn_config.dim
-        dataset = tf.data.experimental.load(dataset_path, element_spec=(
-            tf.TensorSpec(shape=(samples, time_steps, 1)),
-            tf.TensorSpec(shape=(samples, time_steps, dims)),
-            tf.TensorSpec(shape=(samples, time_steps-1, dims)),
-            tf.TensorSpec(shape=(samples, time_steps, 3))))
-        dataset = dataset.batch(self.eqn_config.batch_size)
-        self.solver.fit(x=dataset, epochs=nr_epochs, callbacks=[history])
-        self.solver.no_net.save_weights(checkpoint_path)
-        # return history
-        return history
-    
-class BermudanTrainer(BSDETrainer):
-    def __init__(self, solver: BermudanSolver):
-        super(BermudanTrainer, self).__init__(solver)
-        self.num_tasks = solver.num_tasks
-    
-    def train(self, nr_epochs: int, checkpoint_path: str):
-        self.pre_setting()
-        # Create a callback that saves the model's batch loss
-        class LossHistory(tf.keras.callbacks.Callback):
-            def on_train_begin(self, logs={}):
-                self.losses = []
-            def on_batch_end(self, batch, logs={}):
-                self.losses.append(logs.get('loss'))
-        history = LossHistory()
-        for round in range(10):
-            print(f"========begin round {round + 1}========")
-            self.solver.fit(x=self.data_generator, epochs=nr_epochs, callbacks=[history])
-            self.solver.num_tasks += 1
-            print(f"==========end round {round + 1}========")
-        self.solver.reset_task()
-        self.solver.no_net.save_weights(checkpoint_path)
-        # return history
-        return history
-    
 
 
 class EarlyExerciseSolver:
@@ -212,70 +130,56 @@ class EarlyExerciseSolver:
             self.no_nets[idx].load_weights(checkpoint_path + f"{idx + 1}")
 
 
-    def eval_nets(self, dataset: tf.data.Dataset):
-        # slice a batch
-        ys = []
-        for element in dataset.take(1):
-            t, x, _, u = element
-        for idx in range(len(self.no_nets)):
-            idx_now = self.exercise_index[idx]
-            if idx == len(self.exercise_index)-1:
-                t_s = t[:, :, idx_now:, :]
-                x_s = x[:, :, idx_now:, :]
-                u_s = u[:, :, idx_now:, :]
-            else:
-                idx_fut = self.exercise_index[idx+1]
-                t_s = t[:, :, idx_now:idx_fut, :]
-                x_s = x[:, :, idx_now:idx_fut, :]
-                u_s = u[:, :, idx_now:idx_fut, :]
-            u_c, u_p = self.european_solver.sde.split_uhat(u_s)
-            y_s = self.no_nets[0]((t_s, x_s, u_c, u_p))
-            ys.append(y_s)
-        y = tf.concat(ys, axis=2)
+    # def eval_nets(self, dataset: tf.data.Dataset):
+    #     # slice a batch
+    #     ys = []
+    #     for element in dataset.take(1):
+    #         t, x, _, u = element
+    #     for idx in range(len(self.no_nets)):
+    #         idx_now = self.exercise_index[idx]
+    #         if idx == len(self.exercise_index)-1:
+    #             t_s = t[:, :, idx_now:, :]
+    #             x_s = x[:, :, idx_now:, :]
+    #             u_s = u[:, :, idx_now:, :]
+    #         else:
+    #             idx_fut = self.exercise_index[idx+1]
+    #             t_s = t[:, :, idx_now:idx_fut, :]
+    #             x_s = x[:, :, idx_now:idx_fut, :]
+    #             u_s = u[:, :, idx_now:idx_fut, :]
+    #         u_c, u_p = self.european_solver.sde.split_uhat(u_s)
+    #         y_s = self.no_nets[0]((t_s, x_s, u_c, u_p))
+    #         ys.append(y_s)
+    #     y = tf.concat(ys, axis=2)
         
-        y_exact = self.option.exact_price(t, x, u)
-        return y, y_exact, t, x, u
+    #     y_exact = self.option.exact_price(t, x, u)
+    #     return y, y_exact, t, x, u
     
-    def price(self, x, u):
-        x = tf.convert_to_tensor(x, dtype=tf.float32)
-        t = tf.zeros([1, 1, 1, 1])
-        x = tf.reshape(x, [1, 1, 1, -1])
-        u = tf.reshape(u, [1, 1, 1, -1])
-        if isinstance(self.no_nets[0], DeepONet):
-            y_s = self.no_nets[0]((t, x, u))
-        else:
-            u_c, u_p = self.european_solver.sde.split_uhat(u)
-            y_s = self.no_nets[0]((t, x, u_c, u_p))
-        return y_s.numpy()
+    # def price(self, x, u):
+    #     x = tf.convert_to_tensor(x, dtype=tf.float32)
+    #     t = tf.zeros([1, 1, 1, 1])
+    #     x = tf.reshape(x, [1, 1, 1, -1])
+    #     u = tf.reshape(u, [1, 1, 1, -1])
+    #     if isinstance(self.no_nets[0], DeepONet):
+    #         y_s = self.no_nets[0]((t, x, u))
+    #     else:
+    #         u_c, u_p = self.european_solver.sde.split_uhat(u)
+    #         y_s = self.no_nets[0]((t, x, u_c, u_p))
+    #     return y_s.numpy()
     
-    def price_swap(self, t, x, u):
-        """
-        This method can be used only if self.option is an instance of InterestRateSwap
-        t: [B, M, 1]
-        x: [B, M, 1]
-        u: [B, M, K]
-        e.g. t = [[[0.0]]], x = [[[0.02]]], u = [[[0.5, 0.4, 0.01, 0.011]]]
-        return [[[the value]]]
-        """
-        t_, x_, u_ = tf.expand_dims(t, axis=-1), tf.expand_dims(x, axis=-1), tf.expand_dims(u, axis=2)
-        if isinstance(self.no_nets[0], DeepONet):
-            y_s = self.no_nets[0]((t_, x_, u_))
-        else:
-            u_c, u_p = self.european_solver.sde.split_uhat(u_)
-            y_s = self.no_nets[0]((t_, x_, u_c, u_p))
-        # y_exact = self.option.swap_value(t, x, u)
-        return y_s
-
-            
-
-
-        
-
-
-
-
-
-
-
-
-            
+    # def price_swap(self, t, x, u):
+    #     """
+    #     This method can be used only if self.option is an instance of InterestRateSwap
+    #     t: [B, M, 1]
+    #     x: [B, M, 1]
+    #     u: [B, M, K]
+    #     e.g. t = [[[0.0]]], x = [[[0.02]]], u = [[[0.5, 0.4, 0.01, 0.011]]]
+    #     return [[[the value]]]
+    #     """
+    #     t_, x_, u_ = tf.expand_dims(t, axis=-1), tf.expand_dims(x, axis=-1), tf.expand_dims(u, axis=2)
+    #     if isinstance(self.no_nets[0], DeepONet):
+    #         y_s = self.no_nets[0]((t_, x_, u_))
+    #     else:
+    #         u_c, u_p = self.european_solver.sde.split_uhat(u_)
+    #         y_s = self.no_nets[0]((t_, x_, u_c, u_p))
+    #     # y_exact = self.option.swap_value(t, x, u)
+    #     return y_s
